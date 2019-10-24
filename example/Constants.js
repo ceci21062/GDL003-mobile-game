59bbf4a56b0a70d36818e27e85f8bcc6d6289e75
ceci21062 <ceci21062@gmail.com>
2019-10-24 14:18:46 -0500

    examplo of game snake

---
+++ example/Constants.js
@@ -0,0 +1,8 @@
+import { Dimensions } from 'react-native';
+
+export default Constants = {
+    MAX_WIDTH: Dimensions.get("window").width,
+    MAX_HEIGHT: Dimensions.get("window").height,
+    GRID_SIZE: 15,                  // altura
+    CELL_SIZE: 20                   //anchura
+}
