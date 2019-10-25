d5572713c82b0ea11f01cfbc6e9f6e750d5eb2e6
ceci21062 <ceci21062@gmail.com>
2019-10-25 18:29:17 -0500

    imagen back graund and button

--- example/App.js
+++ example/App.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { AntDesign as Icon } from 'react-native-vector-icons';
+import React, { Component } from 'react';
+import { ImageBackground, StyleSheet, Button, View, Alert } from 'react-native';
+//import {OpcionMenu} from './menu';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-     <Icon name="aliwangwang-o1" size={60} color="#FF99CC" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    let pic = {
+      uri:
+        'https://thumbs.dreamstime.com/b/modelo-inconsútil-de-la-historieta-del-kawaii-con-los-fantasmas-lindos-51223794.jpg',
+    };
+    return (
+      <ImageBackground source={pic} style={styles.imgback}>
+        <View style={styles.menu} />
+        <Button
+          style={styles.button}
+          title="NEW GAME"
+          onPress={() =>
+            Alert.alert(
+              'Alert Title',
+              'My Alert Msg',
+              [
+                {
+                  text: 'Ask me later',
+                  onPress: () => console.log('Ask me later pressed'),
+                },
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+              ],
+              { cancelable: false }
+            )
+          }
+        />
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imgback: {
+    height: '100%',
+  },
+  menu: {
+    height: '40%',
+  },
+  button: {
+    backgroundColor: '#00308F',
   },
 });
+
+export default App;
---
+++ example/menu.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Text, View,Button,TouchableOpacity,StyleSheet } from 'react-native';
+import { FontAwesome as Icon } from 'react-native-vector-icons';
+
+class Menu extends Component {
+    _onPressButton() {
+    alert('You tapped the button!')
+  }
+    render() {
+      return (
+        <View style={{ width: 100, height: 100, alignItems: 'center'}}>
+           <Button
+            onPress={this._onPressButton}
+            title={this.props.name}
+          />
+           <View style={{ width: 50, height: 50, backgroundColor: 'white', alignItems: 'center'}}/>
+        </View>
+      );
+    }
+  }
+  
+ class OpcionMenu extends Component {
+    render() {
+      return (
+        <View style={styles.controls}>
+                    <View style={styles.controlRow}>
+                        <TouchableOpacity 
+                        onPress={() => { this.engine.dispatch({ type: "move-up" })}}>
+                             <Icon name="arrow-up" size={40} color="#00FFFF" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.controlRow}>
+                        <TouchableOpacity onPress={() => { this.engine.dispatch({ type: "move-left" })} }>
+                            <Icon name="arrow-left" size={40} color="#00FFFF" />
+                        </TouchableOpacity>
+                        <View style={[styles.control, { backgroundColor: null}]} />
+                        <TouchableOpacity onPress={() => { this.engine.dispatch({ type: "move-right" })}}>
+                            <Icon name="arrow-right" size={40} color="#00FFFF" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.controlRow}>
+                        <TouchableOpacity onPress={() => { this.engine.dispatch({ type: "move-down" })} }>
+                         <Icon name="arrow-down" size={40} color="#00FFFF" />
+                        </TouchableOpacity>
+                    </View>
+        </View>
+      );
+    }
+  }
+
+  
+
+
+
+
+  const styles = StyleSheet.create({
+    controls: {
+        width: 100,
+        height: 100,
+        flexDirection: 'column',
+    },
+    controlRow: {
+        height: 60,
+        width: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    control: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'black'
+    }
+});
+
+  export default  OpcionMenu;
